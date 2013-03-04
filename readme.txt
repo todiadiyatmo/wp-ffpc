946a8f9f8bf9c788da3936ccb9813b9984086532
cadeyrn <cadeyrn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-04 10:14:30 +0000

    stable tag 0.4.3
    git-svn-id: http://plugins.svn.wordpress.org/wp-ffpc/tags/0.4.3@675985 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,127 @@
+=== WP-FFPC ===
+Contributors: cadeyrn
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=8LZ66LGFLMKJW&lc=HU&item_name=Peter%20Molnar%20photographer%2fdeveloper&item_number=petermolnar%2dpaypal%2ddonation&currency_code=USD&bn=PP%2dDonationsBF%3acredit%2epng%3aNonHosted
+Tags: cache, APC, memcached, full page cache
+Requires at least: 3.0
+Tested up to: 3.5.1
+Stable tag: 0.4.3
+
+Fast Full Page Cache, backend can be memcached or APC
+
+== Description ==
+WP-FFPC is a full page cache plugin for WordPress. It can use APC or a memcached server as backend. 
+The naming stands for Fast Full Page Cache.
+
+PHP has two extension for communication with a memcached server, named Memcache and Memcached. The plugin can utilize both, however, the recommended is memcached.
+
+= Features: =
+* exclude possibility of home, feeds, archieves, pages, singles
+* use APC or memcached as backend
+* 404 caching
+* redirects caching
+* Last Modified HTTP header compatibility with 304 responses
+* shortlink HTTP header preservation
+* pingback HTTP header preservation(1)
+* fallback to no caching if any error or problem occurs
+* Wordpress Network compatible(2)
+* nginx compatible(3)
+* (optional) syslog messages of sets-gets-flushes
+
+(1) pingback hostname will always be generated from the accessed domain, otherwise speed would get highly compromised
+
+(2) If used in WordPress Network, the configuration will only be available for network admins at the network admin panel, and will be system-wide and will be applied for every blog.
+
+(3) nginx compatility means that if used with PHP Memcache or PHP Memcached extension, the created memcached entries can be read and served directly from nginx, making the cache insanely fast.
+If used with APC, this feature is not available (no APC module for nginx), although, naturally, the cache modul is functional and working, but it will be done by PHP instead of nginx.
+Short nginx example configuration is generated on the plugin settings page if Memcache or Memcached is selected as cache type.
+NOTE: some features ( most of additional HTTP headers for example ) will not be available with this solution! ( yet )
+
+Some parts were based on [Hyper Cache](http://wordpress.org/extend/plugins/hyper-cache "Hyper Cache") plugin by Satollo (info@satollo.net).
+
+== Installation ==
+1. Upload contents of `wp-ffpc.zip` to the `/wp-content/plugins/` directory
+2. Enable WordPress cache by adding `define('WP_CACHE',true);` in wp-config.php
+3. Activate the plugin through the `Plugins` menu in WordPress (please use network wide activation if used in a WordPress Network)
+4. Fine tune the settings in `Settings` -> `wp-ffpc` menu in WordPress. For WordPress Network, please visit the Network Admin panel, the options will be available at WP-FFPC menu entry.
+
+== Frequently Asked Questions ==
+
+= How to install memcache PHP extension? =
+On most of the distributions, php5-memcached or php5-mecache is available as package.
+You can use PECL alternatively: `pecl install memcached`.
+
+= How to use the plugin on Amazon Linux? =
+You have to remove the default yum package, named `php-pecl-memcache` and install `Memcache` with PECL.
+
+== Changelog ==
+
+= 0.4.3 =
+2013.03.03
+
+* long-running %3C bug fixed by the help of Mark Costlow <cheeks@swcp.com>, many thanks for it. It was cause by a bad check in the default values set-up: is_numeric applies for string numbers as well, which was unknown to me, and cause some of the values to be 0 where they should have been something different.
+
+= 0.4.2 =
+2012.12.07
+
+* added optional sync protocoll option: replace all http->https or https->http depending on request protocol
+* binary mode is working correctly with memcached extension
+* added warning message for memcache extension in binary mode
+
+KNOWN ISSUES
+
+There are major problems with the "memcache" driver, the source is yet unkown. The situation is that there's no response from the memcached server using this driver; please avoid using it!
+
+= 0.4.1 =
+2012.08.16
+
+* storage key extended with scheme ( http; https; etc. ), the miss caused problems when https request server CSS and JS files via http.
+
+KNOWN ISSUES
+
+
+= 0.4 =
+2012.08.06
+
+* tested against new WordPress versions
+* added lines to "memcached" storage to be able to work with nginx as well
+* added lines to "memcached" to use binary protocol ( tested with PHP Memcached version 2.0.1 )
+
+KNOWN ISSUES
+
+* "memcache" extension fails in binary mode; the reason is under investigation
+
+= 0.3.2 =
+2012.02.27
+
+* apc_cache_info replaced with apc_sma_info, makes plugin faster
+
+= 0.3 =
+2012.02.21
+
+* added syslog debug messages possibility
+* bugfix: removed (accidently used) short_open_tags
+
+= 0.2.3 =
+2012.02.21
+
+* nginx-sample.conf file added, nginx config is created from here
+
+= 0.2.2 =
+2012.02.21
+
+* memcache types bugfix, reported in forum, thanks!
+
+= 0.2.1 =
+2012.02.21
+
+* bugfix, duplicated inclusion could emerge, fix added, thanks for Géza Kuti for reporting!
+
+= 0.2 =
+2012.02.19
+
+* added APC compression option ( requires PHP ZLIB ). Useful is output pages are large. Compression is on lowest level, therefore size/CPU load is more or less optimal.
+
+= 0.1 =
+2012.02.16
+
+* first public release
---
+++ wp-ffpc-common.php
@@ -0,0 +1,249 @@
+<?php
+/**
+ * part of WordPress plugin WP-FFPC
+ */
+
+/**
+ * function to test if selected backend is available & alive
+ */
+global $wp_ffpc_backend;
+global $wp_nmc_redirect;
+
+/* wp ffpc prefic */
+if (!defined('WP_FFPC_PARAM'))
+	define ( 'WP_FFPC_PARAM' , 'wp-ffpc' );
+/* log level */
+define ('WP_FFPC_LOG_LEVEL' , LOG_INFO);
+/* define log ending message */
+define ('WP_FFPC_LOG_TYPE_MSG' , '; cache type: '. $wp_ffpc_config['cache_type'] );
+
+
+/* safety rules if file has been already included */
+if ( function_exists('wp_ffpc_init') || function_exists('wp_ffpc_clear') || function_exists('wp_ffpc_set') || function_exists('wp_ffpc_get') )
+	return false;
+
+/**
+ * init and backend alive check function
+ *
+ * @param $type [optional] if set, alive will be tested against this
+ * 		  if false, backend will be globally initiated
+ * 		  when set, backend will not become global, just tested if alive
+ */
+function wp_ffpc_init( $wp_ffpc_config ) {
+	global $wp_ffpc_backend;
+	$wp_ffpc_backend_status = false;
+
+	if ( empty ( $wp_ffpc_config ))
+		global $wp_ffpc_config;
+
+	/* verify selected storage is available */
+	switch ( $wp_ffpc_config['cache_type'] )
+	{
+		/* in case of apc */
+		case 'apc':
+			/* verify apc functions exist, apc ext is loaded */
+			if (!function_exists('apc_sma_info'))
+				return false;
+			/* verify apc is working */
+			if ( !apc_sma_info() )
+				return false;
+			$wp_ffpc_backend_status = true;
+			break;
+
+		/* in case of Memcache */
+		case 'memcache':
+			/* Memcache class does not exist, Memcache extension is not available */
+			if (!class_exists('Memcache'))
+				return false;
+			if ( $wp_ffpc_backend == NULL )
+			{
+				$wp_ffpc_backend = new Memcache();
+				$wp_ffpc_backend->addServer( $wp_ffpc_config['host'] , $wp_ffpc_config['port'] );
+			}
+			$wp_ffpc_backend_status = $wp_ffpc_backend->getStats( );
+			break;
+
+		/* in case of Memcached */
+		case 'memcached':
+			/* Memcached class does not exist, Memcached extension is not available */
+			if (!class_exists('Memcached'))
+				return false;
+			if ( $wp_ffpc_backend == NULL )
+			{
+				$wp_ffpc_backend = new Memcached();
+				$wp_ffpc_backend->setOption( Memcached::OPT_COMPRESSION , false );
+				$wp_ffpc_backend->setOption( Memcached::OPT_BINARY_PROTOCOL , true );
+				$wp_ffpc_backend->addServer( $wp_ffpc_config['host'] , $wp_ffpc_config['port'] );
+			}
+			$wp_ffpc_backend_status = array_key_exists( $wp_ffpc_config['host'] . ':' . $wp_ffpc_config['port'] , $wp_ffpc_backend->getStats() );
+			break;
+
+		/* cache type is invalid */
+		default:
+			return false;
+	}
+	return $wp_ffpc_backend_status;
+}
+
+/**
+ * clear cache element or flush cache
+ *
+ * @param $post_id [optional] : if registered with invalidation hook, post_id will be passed
+ */
+function wp_ffpc_clear ( $post_id = false ) {
+	global $wp_ffpc_config;
+	global $post;
+
+	$post_only = ( $post_id === 'system_flush' ) ? false : $wp_ffpc_config['invalidation_method'];
+
+	/* post invalidation enabled */
+	if ( $post_only )
+	{
+		$path = substr ( get_permalink($post_id) , 7 );
+		if (empty($path))
+			return false;
+		$wp_ffpc_data_key_protocol = empty ( $_SERVER['HTTPS'] ) ? 'http://' : 'https://';
+		$meta = $wp_ffpc_config['prefix-meta'] . $wp_ffpc_data_key_protocol . $path;
+		$data = $wp_ffpc_config['prefix-data'] . $wp_ffpc_data_key_protocol . $path;
+	}
+
+	switch ($wp_ffpc_config['cache_type'])
+	{
+		/* in case of apc */
+		case 'apc':
+			if ( $post_only )
+			{
+				apc_delete ( $meta );
+				wp_ffpc_log (  ' clearing key: "'. $meta . '"' );
+				apc_delete ( $data );
+				wp_ffpc_log ( ' clearing key: "'. $data . '"' );
+			}
+			else
+			{
+				apc_clear_cache('user');
+				wp_ffpc_log ( ' flushing user cache' );
+				apc_clear_cache('system');
+				wp_ffpc_log ( ' flushing system cache' );
+			}
+			break;
+
+		/* in case of Memcache */
+		case 'memcache':
+		case 'memcached':
+			global $wp_ffpc_backend;
+			if ( $post_only )
+			{
+				$wp_ffpc_backend->delete( $meta );
+				wp_ffpc_log ( ' clearing key: "'. $meta . '"' );
+				$wp_ffpc_backend->delete( $data );
+				wp_ffpc_log ( ' clearing key: "'. $data . '"' );
+			}
+			else
+			{
+				$wp_ffpc_backend->flush();
+				wp_ffpc_log ( ' flushing cache' );
+			}
+			break;
+
+		/* cache type is invalid */
+		default:
+			return false;
+	}
+	return true;
+}
+
+/**
+ * sets a key-value pair in backend
+ *
+ * @param &$key		store key, passed by reference for speed
+ * @param &$data	store value, passed by reference for speed
+ *
+ */
+function wp_ffpc_set ( &$key, &$data, $compress = false ) {
+	global $wp_ffpc_config;
+	global $wp_ffpc_backend;
+
+	$exp = $wp_ffpc_config['user_logged_in'] ? $wp_ffpc_config['expire_member'] : $wp_ffpc_config['expire_visitor'];
+
+	/* syslog */
+	if ($wp_ffpc_config['syslog'])
+	{
+		if ( @is_array( $data ) )
+			$string = serialize($data);
+		else //if ( @is_string( $data ) || @ )
+			$string = $data;
+
+		$size = @strlen($string);
+		wp_ffpc_log ( ' set key: "'. $key . '", size: '. $size . ' byte(s)' );
+	}
+
+	switch ($wp_ffpc_config['cache_type'])
+	{
+		case 'apc':
+			/* use apc_store to overwrite data is existed */
+			if ( $compress )
+				$data = gzdeflate ( $data , 1 );
+			return apc_store( $key , $data , $exp );
+			break;
+		case 'memcache':
+			if ( $wp_ffpc_backend != NULL )
+				/* false to disable compression, vital for nginx */
+				$wp_ffpc_backend->set ( $key, $data , false, $exp );
+			else
+				return false;
+			break;
+		case 'memcached':
+			if ( $wp_ffpc_backend != NULL )
+				$wp_ffpc_backend->set ( $key, $data , $exp );
+			else
+				return false;
+			break;
+	}
+}
+
+/**
+ * gets cached element by key
+ *
+ * @param &$key: key of needed cache element
+ *
+ */
+function wp_ffpc_get( &$key , $uncompress = false ) {
+	global $wp_ffpc_config;
+	global $wp_ffpc_backend;
+
+	/* syslog */
+	wp_ffpc_log ( ' get key: "'.$key . '"' );
+
+	switch ($wp_ffpc_config['cache_type'])
+	{
+		case 'apc':
+			$value = apc_fetch($key);
+			if ( $uncompress )
+				$value = gzinflate ( $value );
+			return $value;
+		case 'memcache':
+		case 'memcached':
+			if ( $wp_ffpc_backend != NULL )
+				return $wp_ffpc_backend->get($key);
+			else
+				return false;
+		default:
+			return false;
+	}
+}
+
+
+/**
+ * handles log messages
+ *
+ * @param $string log messagr
+ */
+function wp_ffpc_log ( $string ) {
+	global $wp_ffpc_config;
+
+	/* syslog */
+	if ($wp_ffpc_config['syslog'] && function_exists('syslog') )
+		syslog( WP_FFPC_LOG_LEVEL , WP_FFPC_PARAM . $string . WP_FFPC_LOG_TYPE_MSG );
+
+}
+?>
